435415cfd0852073b9f74874ae36e1cd4269cea1
Minoo Ghandriz <minooghandriz@gmail.com>
2024-11-07 18:20:19 +0330

    first commit

---
+++ big numbers/BigNumbers.py
@@ -0,0 +1,147 @@
+class BigNumber:
+    def __init__(self , num = None):
+        self.magnitude = []
+
+        if num== None:                        #create an empty bignumber
+            self.sign = True
+            self.magnitude.append(0)
+
+        elif type(num)==str:                 #converts a string to a Big Number
+            if num[0]=='-':
+                self.sign = False
+                n = 1
+            elif num[0] == '+':
+                self.sign = True
+                n = 1
+            else:
+                self.sign = True
+                n = 0
+            for i in range(n , len(num)):
+                    self.magnitude.append(int(num[i]))
+
+        elif type(num)==int:                #converts an integer to a Big Number
+            if num < 0:
+                self.sign = False
+            else:
+                self.sign = True
+            num = abs(num)
+            num_arr = []
+            while num != 0:
+                num_arr.append(num%10)
+                num //= 10                              # for num = 1234, num_arr is [4,3,2,1]
+            self.magnitude = num_arr[::-1].copy()
+
+        else:                             #converts a list of digits to a Big Number
+            if num[0]<0:
+                self.sign = False
+            else:
+                self.sign = True
+            for i in range(len(num)):
+                self.magnitude.append(abs(num[i]))
+
+
+    def add(self, other):
+
+        result = []
+        carry = 0
+        if self.sign == other.sign:
+            first = self.magnitude[::-1].copy()
+            second = other.magnitude[::-1].copy()
+            for i in range(0 , max(len(first) , len(second))):
+                if i<len(first) and i<len(second):
+                    res = first[i] + second[i] + carry
+                    carry = res//10
+                    result.append(res%10)
+                elif i >= len(first):
+                    res = second[i] + carry
+                    carry = res // 10
+                    result.append(res%10)
+                elif i >= len(second):
+                    res = first[i] + carry
+                    carry = res // 10
+                    result.append(res % 10)
+            if carry != 0:
+                result.append(carry)
+
+            self.magnitude =  result[::-1].copy()             #if you want your main Bignumber not to change, ignore this line and return result[::-1] instead
+            return self.magnitude
+
+        else:     #if their signs are different
+
+            first = []
+            second = []
+
+            if len(self.magnitude) > len(other.magnitude):
+                first = self.magnitude[::-1].copy()
+                second = other.magnitude[::-1].copy()
+            elif len(self.magnitude) == len(other.magnitude):
+                flag = False
+                for i in range(len(self.magnitude)):
+                    if self.magnitude[i] == other.magnitude[i]:
+                        continue
+                    elif self.magnitude[i] < other.magnitude[i]:
+                        first = other.magnitude[::-1].copy()
+                        second = self.magnitude[::-1].copy()
+                        self.sign = other.sign
+                        flag = True
+                        break
+                    else:
+                        break
+
+                if flag == False:
+                    first = self.magnitude[::-1].copy()
+                    second = other.magnitude[::-1].copy()
+
+            else:
+                first = other.magnitude[::-1].copy()
+                second = self.magnitude[::-1].copy()
+                self.sign = other.sign
+
+
+            for i in range(len(first)):
+                if first[i] < second[i]:
+                    first[i+1] -= 1
+                    result.append(first[i]+10-second[i])
+
+                else:
+                    result.append(first[i]-second[i])
+
+            self.magnitude = result[::-1].copy()
+            return self.magnitude
+
+
+    def sub(self , other):
+
+        if self.sign != other.sign:
+            bignum = BigNumber(other.magnitude)
+            bignum.sign = self.sign
+
+            return self.add(bignum)
+
+        else:
+            bignum = BigNumber(other.magnitude)
+            if other.sign == True:
+                bignum.sign = False
+            else:
+                bignum.sign = True
+
+            return self.add(bignum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+b1 = BigNumber(1274)
+b2 = BigNumber('8754')
+
+print(b1.magnitude , b1.sign)
+print(b2.magnitude, b2.sign)
+print(b1.sub(b2) , b1.sign)
