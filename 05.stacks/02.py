d39401461e7382c4b7eab29855f6875607bee888
Minoo Ghandriz <minooghandriz@gmail.com>
2024-11-18 10:49:33 +0330

    changes

--- 05.stacks/02.py
+++ 05.stacks/02.py
@@ -1,9 +1,9 @@
-queue = ["a","b","c","d","j","k","l"]
-stack = ["c","b","a","d","e","f"]
+queue = [1,2,3,4,5,6]
+stack = [3,5,7,8,1,4]
 intercept = []
 for i in range(len(queue)):
     for j in range(len(queue)):
-        if queue[0] in stack[0]:
+        if queue[0] == stack[0]:
             inter = stack.pop(0)
             queue.pop(0)
             intercept.append(inter)
